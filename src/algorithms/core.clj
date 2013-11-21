227aaeddba6d3c0a5b57c8e6fd6de6cb1c5b68cc
David Rupp <david@ruppworks.com>
2013-11-21 12:46:16 -0500

    Cleaned up pow1 and pow2. Added matrix multiplication, matrix algorithm for fibonacci sequence.

--- src/algorithms/core.clj
+++ src/algorithms/core.clj
@@ -1,31 +1,51 @@
-(ns algorithms.core)
+(ns algorithms.core
+  (:refer-clojure :exclude [* - + == /])
+  (:use clojure.core.matrix)
+  (:use clojure.core.matrix.operators))
 
 (defn gcd [a b]
   (if (= b 0)
     a
     (recur b (mod a b))))
 
-(defn pow [n exp]
+(defn pow1 [n exp]
   "naive version O(n)"
   (if (= 0 exp)
     1
-    (*' n (pow n (dec exp)))))
+    (*' n (pow1 n (dec exp)))))
+
+(defn- nexp [exp]
+  (if (even? exp)
+    (/ exp 2)
+    (/ (dec exp) 2)))
 
 (defn pow2 [n exp]
   "better version O(lg n)"
   (if (= 0 exp)
     1
-    (if (even? exp)
-      (let [mem (pow2 n (/ exp 2))]
-        (*' mem mem))
-      (let [mem (pow2 n (/ (- exp 1) 2))]
-        (*' mem mem n)))))
+    (let [mem (pow2 n (nexp exp))]
+      (*' mem mem (if (even? exp) 1 n)))))
+
+(defn mpow [m exp]
+  (let [ident (identity-matrix 2)]
+    (if (= 0 exp)
+      ident
+      (let [mem (mpow m (nexp exp))]
+        (mmul mem mem (if (even? exp) ident m))))))
 
 (def fib-seq
   (iterate (fn [[x y]] [y (+' x y)]) [0 1]))
 
 (def fib
   (map first fib-seq))
+
+(def mfib-seq
+  (let [z [[0 0] [0 0]]
+        f (matrix [[1 1] [1 0]])]
+    (lazy-seq (cons z (iterate (partial mmul f) f)))))
+
+(def mfib
+  (map #(second (first %)) mfib-seq))
 
 (defn- prngf 
   "Pseudo-random number generating function, as in Essential Algorithms, Chapter 2"
